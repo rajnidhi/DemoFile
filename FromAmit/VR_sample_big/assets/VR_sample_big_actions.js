9077eda15d54563eadd080c1c4c1b40343cd7eca
Nidhi <nid@nid-macos.macromedia.com>
2017-11-24 14:24:39 +0530

    amit sample contents

---
+++ FromAmit/VR_sample_big/assets/VR_sample_big_actions.js
@@ -0,0 +1,21 @@
+var flwebgl;
+(function (flwebgl) {
+	(function (actions) {
+		actions.mc_huge_20_138 = function() {
+			/* gotoAndPlay("special board", 1);
+			*/
+		}
+		actions.mc_huge_20_193 = function() {
+			/* gotoAndPlay("sunset", 1);
+			*/
+		}
+		actions.mc_bowl32blue_1_0 = function() {
+			/* stop();
+			*/
+		}
+		actions.mc_bowl32blue_1_1 = function() {
+			/* stop();
+			*/
+		}
+	})(flwebgl.actions || (flwebgl.actions = {}));
+})(flwebgl || (flwebgl = {}));
